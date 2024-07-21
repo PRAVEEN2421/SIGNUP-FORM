89c9e133456fb119a3c816df2dd0214a1b6d4c56
Sai Praveen <162142454+PRAVEEN2421@users.noreply.github.com>
2024-07-21 20:17:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Formtask from './singutask';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <Formtask/>
+    </div>
+  );
+}
+
+export default App;
